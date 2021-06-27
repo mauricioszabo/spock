aa4cff8a27249e7c28760cbc82331798eb689b21
Maurício Szabo <mauricio.szabo@gmail.com>
2021-06-27 10:21:51 -0300

    Fixed Project.CLJ

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject spock "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject link.szabo.mauricio/spock "0.1.0"
+  :description "Wrapper for TuProlog and SWI-Prolog in Clojure"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "2-Clause BSD"
+            :url "https://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-
-  :resource-paths ["resources/jpl.jar"]
-  :jvm-opts [~(str "-Djava.library.path=resources/:" (System/getProperty "java.library.path"))]
 
   :repl-options {:init-ns user}
   :profiles {:dev {:dependencies [[check "0.2.0-SNAPSHOT"]
                                   [it.unibo.tuprolog/solve-jvm "0.17.4-dev09+485a46b2"]
-                                  [it.unibo.tuprolog/solve-classic-jvm "0.17.4-dev09+485a46b2"]]}})
+                                  [it.unibo.tuprolog/solve-classic-jvm "0.17.4-dev09+485a46b2"]]
+                   :resource-paths ["resources/jpl.jar"]
+                   :jvm-opts [~(str "-Djava.library.path=resources/:" (System/getProperty "java.library.path"))]}})
